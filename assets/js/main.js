c0232f099869288c455c073ac57afad0730c9b1a
Carloshlc007 <carloshlc@hotmail.com>
2024-02-09 12:28:59 -0300

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,12 @@ function updateProfileData(profileData){
     const photo = document.getElementById("profile.photo")
     photo.src = profileData.photo
     photo.alt = profileData.name
+
+    const nome = document.getElementById("profile.name")
+    const job = document.getElementById("profile.job")
+    const location = document.getElementById("profile.location")
+    const phone = document.getElementById("profile.phone")
+    const email = document.getElementById("profile.email")
 }
 
 
