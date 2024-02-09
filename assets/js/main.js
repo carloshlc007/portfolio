16548f92b874eb34a6c5e8bfd64e9f0c2b24c4d8
Carloshlc007 <carloshlc@hotmail.com>
2024-02-09 12:19:31 -0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
+
+function updateProfileData(profileData){
+    const photo = document.getElementById("profile.photo")
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+}
+
+
+
 (async () =>{
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileData(profileData)
 })()
